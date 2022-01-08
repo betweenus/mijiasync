c2ad9d983e23c6a711412ca5b5ab799c2257cde2
betweenus <34419426+betweenus@users.noreply.github.com>
2022-01-09 01:18:13 +0300

    Add Syslog support, fixing disconnects and errors

--- mijiasync.py
+++ mijiasync.py
@@ -1,11 +1,19 @@
 from bluepy.btle import Peripheral
 import time
 import struct
+import syslog
 
 devices = [  # list of clock devices (mac addresses)
     'e7:2e:00:00:00:00',    # LYWSD02
     'e7:50:59:00:00:00'     # MHO-C303
 ]
+
+log2syslog = True
+
+def tolog(device, msg):
+    print(device, msg)
+    if log2syslog:
+        syslog.syslog(device + ': ' + msg)
 
 def rawtime():
     gmtoff = time.localtime().tm_gmtoff
@@ -18,22 +26,28 @@ def rawtime():
     return struct.pack('<Ib', ctime, gmtoffh)
 
 for device in devices:
+    try:
+        per = Peripheral(device)
+    except Exception as e:
+        tolog(device, 'Error when connect to clock!')
+        continue
+    
     ch = []
     
-    for i in range(10):
-        try:
-            per = Peripheral(device)
-            ch = per.getCharacteristics(uuid = 'ebe0ccb7-7a0a-4b0c-8a1a-6ff2997da3a6')
-            break
-        except BTLEDisconnectError:
-            time.sleep(5)
-            continue
+    try:
+        ch = per.getCharacteristics(uuid = 'ebe0ccb7-7a0a-4b0c-8a1a-6ff2997da3a6')
+    except Exception as e:
+        pass
     
     if len(ch) != 1:
+        tolog(device, 'Error when get characteristics!')
+        per.disconnect()
         continue
     
     if ch[0].read() != rawtime():
-        print(device, 'Syncing...')
+        tolog(device, 'Synchronizing...')
         ch[0].write(rawtime())
     else:
-        print(device, 'No need to sync.')
+        tolog(device, 'No need to sync')
+        
+    per.disconnect()
